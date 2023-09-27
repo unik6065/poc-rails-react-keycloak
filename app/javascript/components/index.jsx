fa99a83c91a74e6bc14c11fb7a25a93b34dec2e4
romain isoz <isozromain@gmail.com>
2023-09-27 22:31:20 +0200

    add auth logic within homepage

--- app/javascript/components/index.jsx
+++ app/javascript/components/index.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import { createRoot } from 'react-dom/client'
+import { AuthProvider } from "react-oidc-context";
 import App from './App'
+
+const oidcConfig = {
+  authority: "http://localhost:8080/realms/SecondRealm",
+  client_id: "localApp",
+  redirect_uri: "http://localhost:3000/"
+}
 
 document.addEventListener('turbo:load', () => {
   const root = createRoot(
     document.body.appendChild(document.createElement('div'))
   );
-  root.render(<App />)
+  root.render(
+    <>
+      <AuthProvider {...oidcConfig}>
+        <App />
+      </AuthProvider>
+    </>
+  )
 });
--- app/javascript/pages/HomePage.jsx
+++ app/javascript/pages/HomePage.jsx
@@ -1,9 +1,27 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { useAuth } from "react-oidc-context"
 
 function HomePage() {
+  const auth = useAuth();
+
+  useEffect(() => {
+    auth.signinSilent();
+  }, [])
+
+  let button;
+
+  if (auth.isAuthenticated) {
+    button = (
+      <button onClick={() => void auth.signoutSilent()} >logout</button>
+    )
+  } else {
+    button = (<button onClick={() => void auth.signinRedirect()}>login</button>)
+  }
   return (
     <>
+      {auth.user?.profile.preferred_username}
       <h1>Welcome on the homepage of my app</h1>
+      {button}
     </>
   )
 }
